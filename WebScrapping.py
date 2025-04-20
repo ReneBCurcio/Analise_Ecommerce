70ab6711c896624d52127cbb9c05492d89fc3bcc
ReneBCurcio <renecbastos@outlook.com>
2025-04-20 04:09:45 -0300

    Achar o elemento referente ao preço no site

--- WebScrapping.py
+++ WebScrapping.py
@@ -5,6 +5,7 @@ from selenium.webdriver.edge.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.common.by import By
+import pandas as pd
 
 edge_options = Options()
 edge_options.add_argument('--headless')  # Roda sem abrir o navegador
@@ -18,9 +19,10 @@ caminho = "https://www.amazon.com.br/s?k=monitor+27+polegadas&crid=2BXGDPJX7837C
 
 driver.get(caminho)
 elem = driver.find_elements(By.TAG_NAME, "h2")
-elem_2 = driver.find_elements(By.TAG_NAME, "a-price-whole")
 
+elem_2 = driver.find_elements(By.CLASS_NAME, "a-price-whole")
 
-for h2 in elem_2:
-    print(h2.text)
+df = pd.DataFrame(elem_2)
+print(df)
+
 driver.quit()
