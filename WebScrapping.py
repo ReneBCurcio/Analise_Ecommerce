3abf9f12080fcf7a396fde82500bc144ddc237f2
ReneBCurcio <renecbastos@outlook.com>
2025-04-20 03:58:59 -0300

    Elemento h2 do site

--- WebScrapping.py
+++ WebScrapping.py
@@ -17,7 +17,10 @@ driver = webdriver.Edge(service=service, options=edge_options)
 caminho = "https://www.amazon.com.br/s?k=monitor+27+polegadas&crid=2BXGDPJX7837C&sprefix=mON%2Caps%2C342&ref=nb_sb_ss_ts-doa-p_4_3"
 
 driver.get(caminho)
-elem = driver.find_element(By.CSS_SELECTOR, "h3")
-print(elem)
+elem = driver.find_elements(By.TAG_NAME, "h2")
+elem_2 = driver.find_elements(By.TAG_NAME, "a-price-whole")
 
+
+for h2 in elem_2:
+    print(h2.text)
 driver.quit()
